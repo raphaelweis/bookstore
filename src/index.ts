2a8eac9fbeaf192bd208adc89255df336abcb3bd
Raphaël Weis <raphael.weis@loreal.com>
2025-09-05 13:30:08 +0200

    feat: Added the morgan logger as middleware.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import users from "./routes/users";
+import morgan from "morgan";
 
 const PORT = 3000;
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(morgan("dev"));
+
+// Routers
 app.use("/users", users);
 
 app.get(`/`, async (_req, res) => {
