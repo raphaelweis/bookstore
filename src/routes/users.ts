98e3006ca6f4f43fc6553cc634f1e4cd01041a26
Raphaël Weis <raphael.weis@loreal.com>
2025-09-05 11:43:08 +0200

    feat: Added fake CRUD operations for the users router.

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,7 @@
 import express from "express";
+import { User } from "../types.ts";
 
 const router = express.Router();
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  created_at: Date;
-  updated_at: Date;
-}
 
 const users: User[] = [
   {
@@ -19,14 +12,14 @@ const users: User[] = [
     updated_at: new Date(),
   },
   {
-    id: 0,
+    id: 1,
     name: "Alice",
     email: "alice@mail.com",
     created_at: new Date(),
     updated_at: new Date(),
   },
   {
-    id: 0,
+    id: 2,
     name: "Bob",
     email: "bob@mail.com",
     created_at: new Date(),
@@ -34,8 +27,66 @@ const users: User[] = [
   },
 ];
 
-router.get(`/`, (_req, res) => {
-  res.send(users);
+/**
+ * Get all users
+ */
+router.get(`/`, (_req, res) => res.send(users));
+
+/**
+ * Get a user by ID
+ */
+router.get(`/:userId`, (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const userResponse = users.filter((user) => userId === user.id);
+
+  res.send(userResponse);
+});
+
+/**
+ * Add a new user
+ */
+router.post(`/`, (req, res) => {
+  const newUser = {
+    id: 3,
+    name: req.body.name,
+    email: req.body.email,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+  users.push(newUser);
+  res.send(newUser);
+});
+
+/**
+ * Update email and name for an existing user (by ID)
+ */
+router.put(`/:userId`, (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const index = users.findIndex((user) => userId === user.id);
+  const userToUpdate = users[index];
+
+  const updatedUser = {
+    ...userToUpdate,
+    name: req.body.email,
+    email: req.body.email,
+  };
+
+  users[index] = updatedUser;
+  res.send(updatedUser);
+});
+
+/**
+ * Delete a user by ID
+ */
+router.delete(`/:userId`, (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  const index = users.findIndex((user) => userId === user.id);
+
+  const removedUser = users[index];
+  users.splice(index, 1);
+
+  res.send(removedUser);
 });
 
 export default router;
