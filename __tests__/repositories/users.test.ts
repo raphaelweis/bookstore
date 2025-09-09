541417559deec145f46e7428268c89ba645a995e
Raphaël Weis <raphael.weis@loreal.com>
2025-09-09 18:55:59 +0200

    test: Added a test to check for 404 error in addUser.

--- __tests__/repositories/users.test.ts
+++ __tests__/repositories/users.test.ts
@@ -1,5 +1,8 @@
+import { PrismaClientKnownRequestError } from "../../generated/prisma/runtime/library";
+import { BookstoreError } from "../../src/middlewares/errors";
 import * as userRepository from "../../src/repositories/users";
 import { prismaMock } from "../../src/singleton";
+import { HTTPErrorCodes } from "../../src/types";
 
 test("should create a new user", async () => {
   const now = new Date();
@@ -24,4 +27,31 @@ test("should create a new user", async () => {
     created_at: now,
     updated_at: now,
   });
+});
+
+test("should fail when the email is already in the database", async () => {
+  const createUserData = {
+    name: "Raph",
+    email: "raph@mail.com", // This email is supposed to exist already
+  };
+
+  const mockError = new PrismaClientKnownRequestError(
+    "A user with email: 'raph@mail.com' already exists.",
+    {
+      code: "P2002",
+      clientVersion: "x.y.z",
+      meta: {
+        target: ["email"],
+      },
+    },
+  );
+
+  prismaMock.user.create.mockRejectedValue(mockError);
+
+  await expect(userRepository.addUser(createUserData)).rejects.toThrow(
+    new BookstoreError(
+      HTTPErrorCodes.CONFLICT,
+      `A user with email: 'raph@mail.com' already exists.`,
+    ),
+  );
 });
